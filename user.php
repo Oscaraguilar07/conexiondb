e6272271bf61a8978825b9f21a4811f806943d35
Oscaraguilar07 <125697206+Oscaraguilar07@users.noreply.github.com>
2023-10-17 15:46:23 -0500

    Add files via upload

---
+++ user.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'database.php';
+$query = "select * from productos";
+if ($is_query_run = mysqli_query($conn, $query)){
+    $userData = [];
+    while ($query_executed = mysqli_fetch_assoc($is_query_run)){
+        $userData[] = $query_executed;
+    }
+}
+
+else{
+    echo "Error in execution!";
+}
+
+echo json_encode($userData);
+
+
+?>
